805d00a4f73c4722aea8cc73e347d15cc0311996
Oksana <ereminaoksana82@yandex.ru>
2022-09-08 21:58:40 +0400

    Task29

--- HW4_task27/Program.cs
+++ HW4_task27/Program.cs
@@ -1,28 +1,28 @@
-﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+﻿    // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
 
-// 452 -> 11
+    // 452 -> 11
 
-// 82 -> 10
+    // 82 -> 10
 
-// 9012 -> 12
+    // 9012 -> 12
 
 
-Console.Write("Введите число: ");
-int numberN = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите число: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
 
-  int SumNumber(int numberN){
-    
-    int counter = Convert.ToString(numberN).Length;
-    int advance = 0;
-    int result = 0;
+    int SumNumber(int numberN){
+        
+        int counter = Convert.ToString(numberN).Length;
+        int advance = 0;
+        int result = 0;
 
-    for (int i = 0; i < counter; i++){
-      advance = numberN - numberN % 10;
-      result = result + (numberN - advance);
-      numberN = numberN / 10;
+        for (int i = 0; i < counter; i++){
+        advance = numberN - numberN % 10;
+        result = result + (numberN - advance);
+        numberN = numberN / 10;
+        }
+    return result;
     }
-   return result;
-  }
 
-int sumNumber = SumNumber(numberN);
-Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+    int sumNumber = SumNumber(numberN);
+    Console.WriteLine("Сумма цифр в числе: " + sumNumber);
---
+++ HW4_task29/Program.cs
@@ -0,0 +1,29 @@
+﻿ // Задача 29: Напишите программу, которая задаёт массив из введенного числа и выводит их на экран;
+ // пользователь вводит число элементов в массиве и минимальное, и максимальное значение;
+ // надо заполнить массив рандоными данными от минимального до максимального значение. И важное уточнение, делать все в методе,
+ // который возвращает массив.
+ // 5, 1, 20 -> [1, 2, 5, 7, 19]
+
+ // 3, 1, 33 -> [6, 1, 33]
+
+    int[] createRandomArray(int length, int minValue, int maxValue) 
+    {
+    
+        int[] result = new int[length];
+    
+    Random objectOfRandomClass = new Random();
+    for (int i = 0; i < length; ++i) {
+            
+            int randomNumber = objectOfRandomClass.Next(minValue, maxValue);
+    result[i] = randomNumber;
+        }
+        return result;
+    }
+    Console.WriteLine("Введите количество чисел: ");
+        int length = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите минимально возможное число в массиве: ");
+        int minValue = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите максимально возможное число в массиве: ");
+        int maxValue = Convert.ToInt32(Console.ReadLine());
+        int[] result = createRandomArray(length, minValue, maxValue);
+        Console.WriteLine("Результат: [{0}]",string.Join(", ",result));
